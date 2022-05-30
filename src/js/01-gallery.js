a0cd19e35ad52350a308b365d117b9a20f8fb278
Siarhey Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-05-30 22:30:01 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,31 @@
 import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+
+const galleryEl = document.querySelector(".gallery");
+
+const galleryItemsEl = galleryItems ;
+
+console.log(galleryItemsEl)
+
+const markup = galleryItemsEl.map(({ preview, original, description }) => {
+   return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`}).join("");
+
+console.log(markup);
+
+galleryEl.insertAdjacentHTML("afterbegin", markup);
+
+//lib
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
 
+
+
+    const iframe = document.querySelector('#vimeo-player');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+
+    const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
