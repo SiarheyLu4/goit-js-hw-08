f8d385f9e31ca1ed7e546d2bf4b08d71c1c8d82a
Siarhey Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-05-31 12:28:17 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,21 +1,23 @@
 import Player from '@vimeo/player';
-
+import throttle from 'lodash.throttle';
 
 
     const iframe = document.querySelector('#vimeo-player');
-    const player = new Vimeo.Player(iframe);
+    const player = new Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+const onPlay = function(data) {
+    const value = data.seconds;
+    console.log(value);
+    localStorage.setItem("videoplayer-current-time", value);
+    };
+    player.on('timeupdate', throttle(onPlay, 1000));
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-
-
-    const onPlay = function(data) {
-    // data is an object containing properties specific to that event
-};
-
-player.on('play', onPlay);
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
